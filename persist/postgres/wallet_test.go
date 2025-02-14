42772dd397df52e67688a068068a669de66f83e3
PJ <peterjan.brone@gmail.com>
2025-02-14 09:55:25 +0100

    db: address review remarks

--- persist/postgres/wallet_test.go
+++ persist/postgres/wallet_test.go
@@ -10,122 +10,107 @@ import (
 	"go.uber.org/zap/zaptest"
 )
 
-func TestSingleAddressWalletStore(t *testing.T) {
+func TestSingleAddressWalletStoreTip(t *testing.T) {
 	store := initPostgres(t, zaptest.NewLogger(t).Named("postgres"))
 
-	for _, m := range []struct {
-		method string
-		test   func(*Store) func(*testing.T)
-	}{
-		{"Tip", testSingleAddressWalletStoreTip},
-		{"UnspentSiacoinElements", testSingleAddressWalletStoreUnspentSiacoinElements},
-		{"WalletEvents/WalletEventCount", testSingleAddressWalletStoreWalletEventsAndWalletEventCount},
-	} {
-		t.Run(m.method, m.test(store))
+	if _, err := store.pool.Exec(context.Background(), `UPDATE global_settings SET last_scanned_index = NULL`); err != nil {
+		t.Fatal(err)
+	}
+
+	ci, err := store.Tip()
+	if err != nil {
+		t.Fatal(err)
+	} else if ci != (types.ChainIndex{}) {
+		t.Fatal("unexpected tip", ci)
+	}
+
+	update := types.ChainIndex{Height: 1, ID: types.BlockID{1}}
+	if _, err := store.pool.Exec(context.Background(), `UPDATE global_settings SET last_scanned_index = $1`, sqlChainIndex(update)); err != nil {
+		t.Fatal(err)
+	}
+
+	ci, err = store.Tip()
+	if err != nil {
+		t.Fatal(err)
+	} else if ci != update {
+		t.Fatal("unexpected tip", ci)
 	}
 }
 
-func testSingleAddressWalletStoreTip(store *Store) func(t *testing.T) {
-	return func(t *testing.T) {
-		if _, err := store.pool.Exec(context.Background(), `UPDATE global_settings SET last_scanned_index = NULL`); err != nil {
-			t.Fatal(err)
-		}
+func TestSingleAddressWalletStoreUnspentSiacoinElements(t *testing.T) {
+	store := initPostgres(t, zaptest.NewLogger(t).Named("postgres"))
 
-		ci, err := store.Tip()
-		if err != nil {
-			t.Fatal(err)
-		} else if ci != (types.ChainIndex{}) {
-			t.Fatal("unexpected tip", ci)
-		}
+	if utxos, err := store.UnspentSiacoinElements(); err != nil {
+		t.Fatal(err)
+	} else if len(utxos) != 0 {
+		t.Fatal("unexpected number of utxos", len(utxos))
+	}
 
-		update := types.ChainIndex{Height: 1, ID: types.BlockID{1}}
-		if _, err := store.pool.Exec(context.Background(), `UPDATE global_settings SET last_scanned_index = $1`, sqlChainIndex(update)); err != nil {
-			t.Fatal(err)
-		}
+	update := newTestSiacoinElement()
+	if _, err := store.pool.Exec(context.Background(), `INSERT INTO wallet_siacoin_elements (output_id, value, address, merkle_proof, leaf_index, maturity_height) VALUES ($1, $2, $3, $4, $5, $6)`,
+		sqlHash256(update.ID),
+		sqlCurrency(update.SiacoinOutput.Value),
+		sqlHash256(update.SiacoinOutput.Address),
+		sqlMerkleProof(update.StateElement.MerkleProof),
+		update.StateElement.LeafIndex,
+		update.MaturityHeight,
+	); err != nil {
+		t.Fatal(err)
+	}
 
-		ci, err = store.Tip()
-		if err != nil {
-			t.Fatal(err)
-		} else if ci != update {
-			t.Fatal("unexpected tip", ci)
-		}
+	if utxos, err := store.UnspentSiacoinElements(); err != nil {
+		t.Fatal(err)
+	} else if len(utxos) != 1 {
+		t.Fatal("unexpected number of utxos", len(utxos))
+	} else if !reflect.DeepEqual(utxos[0], update) {
+		t.Fatal("unexpected utxo", utxos[0])
 	}
 }
 
-func testSingleAddressWalletStoreUnspentSiacoinElements(store *Store) func(t *testing.T) {
-	return func(t *testing.T) {
-		if utxos, err := store.UnspentSiacoinElements(); err != nil {
-			t.Fatal(err)
-		} else if len(utxos) != 0 {
-			t.Fatal("unexpected number of utxos", len(utxos))
-		}
+func TestSingleAddressWalletStoreWalletEventsAndWalletEventCount(t *testing.T) {
+	store := initPostgres(t, zaptest.NewLogger(t).Named("postgres"))
 
-		update := newTestSiacoinElement()
-		if _, err := store.pool.Exec(context.Background(), `INSERT INTO wallet_siacoin_elements (output_id, value, address, merkle_proof, leaf_index, maturity_height) VALUES ($1, $2, $3, $4, $5, $6)`,
-			sqlHash256(update.ID),
-			sqlCurrency(update.SiacoinOutput.Value),
-			sqlHash256(update.SiacoinOutput.Address),
-			sqlMerkleProof(update.StateElement.MerkleProof),
-			update.StateElement.LeafIndex,
-			update.MaturityHeight,
-		); err != nil {
-			t.Fatal(err)
-		}
-
-		if utxos, err := store.UnspentSiacoinElements(); err != nil {
-			t.Fatal(err)
-		} else if len(utxos) != 1 {
-			t.Fatal("unexpected number of utxos", len(utxos))
-		} else if !reflect.DeepEqual(utxos[0], update) {
-			t.Fatal("unexpected utxo", utxos[0])
-		}
+	if events, err := store.WalletEvents(0, 1); err != nil {
+		t.Fatal(err)
+	} else if len(events) != 0 {
+		t.Fatal("unexpected number of events", len(events))
 	}
-}
 
-func testSingleAddressWalletStoreWalletEventsAndWalletEventCount(store *Store) func(t *testing.T) {
-	return func(t *testing.T) {
-		if events, err := store.WalletEvents(0, -1); err != nil {
-			t.Fatal(err)
-		} else if len(events) != 0 {
-			t.Fatal("unexpected number of events", len(events))
-		}
+	if count, err := store.WalletEventCount(); err != nil {
+		t.Fatal(err)
+	} else if count != 0 {
+		t.Fatal("unexpected number of events", count)
+	}
 
-		if count, err := store.WalletEventCount(); err != nil {
-			t.Fatal(err)
-		} else if count != 0 {
-			t.Fatal("unexpected number of events", count)
-		}
+	update := newTestEvent()
+	if _, err := store.pool.Exec(context.Background(), `INSERT INTO wallet_events (chain_index, maturity_height, event_id, event_type, event_data) VALUES ($1, $2, $3, $4, $5)`,
+		sqlChainIndex(update.Index),
+		update.MaturityHeight,
+		sqlHash256(update.ID),
+		update.Type,
+		sqlEncodeEvent(update.Type, update.Data),
+	); err != nil {
+		t.Fatal(err)
+	}
 
-		update := newTestEvent()
-		if _, err := store.pool.Exec(context.Background(), `INSERT INTO wallet_events (chain_index, maturity_height, event_id, event_type, event_data) VALUES ($1, $2, $3, $4, $5)`,
-			sqlChainIndex(update.Index),
-			update.MaturityHeight,
-			sqlHash256(update.ID),
-			update.Type,
-			sqlEncodeEvent(update.Type, update.Data),
-		); err != nil {
-			t.Fatal(err)
-		}
+	if count, err := store.WalletEventCount(); err != nil {
+		t.Fatal(err)
+	} else if count != 1 {
+		t.Fatal("unexpected number of events", count)
+	}
 
-		if count, err := store.WalletEventCount(); err != nil {
-			t.Fatal(err)
-		} else if count != 1 {
-			t.Fatal("unexpected number of events", count)
-		}
+	if events, err := store.WalletEvents(0, 1); err != nil {
+		t.Fatal(err)
+	} else if len(events) != 1 {
+		t.Fatal("unexpected number of events", len(events))
+	} else if !reflect.DeepEqual(events[0], update) {
+		t.Fatal("unexpected event", update, events[0])
+	}
 
-		if events, err := store.WalletEvents(0, -1); err != nil {
-			t.Fatal(err)
-		} else if len(events) != 1 {
-			t.Fatal("unexpected number of events", len(events))
-		} else if !reflect.DeepEqual(events[0], update) {
-			t.Fatal("unexpected event", update, events[0])
-		}
-
-		if events, err := store.WalletEvents(1, -1); err != nil {
-			t.Fatal(err)
-		} else if len(events) != 0 {
-			t.Fatal("unexpected number of events", len(events))
-		}
+	if events, err := store.WalletEvents(1, 1); err != nil {
+		t.Fatal(err)
+	} else if len(events) != 0 {
+		t.Fatal("unexpected number of events", len(events))
 	}
 }
 
