8c1c855fe86569e1261108f3fe9577ac95afb2b2
PJ <peterjan.brone@gmail.com>
2025-06-03 12:18:38 +0200

    gh: fix diff

--- persist/postgres/contracts.go
+++ persist/postgres/contracts.go
@@ -377,6 +377,39 @@ func (s *Store) MarkPruned(ctx context.Context, contractID types.FileContractID)
 	})
 }
 
+// MarkUnrenewableContractsBad marks all contracts as bad that have a proof
+// height <= minProofHeight bad.
+func (s *Store) MarkUnrenewableContractsBad(ctx context.Context, minProofHeight uint64) error {
+	return s.transaction(ctx, func(ctx context.Context, tx *txn) error {
+		_, err := tx.Exec(ctx, `UPDATE contracts SET good = FALSE WHERE proof_height <= $1`, minProofHeight)
+		return err
+	})
+}
+
+// RejectPendingContracts marks all contracts as rejected that are currently
+// pending and have a formation height older than 'maxFormation'.
+func (s *Store) RejectPendingContracts(ctx context.Context, maxFormation time.Time) error {
+	return s.transaction(ctx, func(ctx context.Context, tx *txn) error {
+		_, err := tx.Exec(ctx, `UPDATE contracts SET state = $1 WHERE state = $2 AND formation < $3`,
+			sqlContractState(contracts.ContractStateRejected), sqlContractState(contracts.ContractStatePending), maxFormation)
+		return err
+	})
+}
+
+// SyncContract updates the contract with the given ID to the provided
+// parameters which are expected to contain information about the latest
+// revision of a contract.
+func (s *Store) SyncContract(ctx context.Context, contractID types.FileContractID, params contracts.ContractSyncParams) error {
+	return s.transaction(ctx, func(ctx context.Context, tx *txn) error {
+		_, err := tx.Exec(ctx, `
+UPDATE contracts
+SET capacity = $1, remaining_allowance = $2, revision_number = $3, size = $4, used_collateral = $5
+WHERE contract_id = $6
+`, params.Capacity, sqlCurrency(params.RemainingAllowance), params.RevisionNumber, params.Size, sqlCurrency(params.UsedCollateral), sqlHash256(contractID))
+		return err
+	})
+}
+
 // PrunableContractRoots diffs the given roots with the roots in the database
 // and returns the roots that can be pruned.
 func (s *Store) PrunableContractRoots(ctx context.Context, contractID types.FileContractID, roots []types.Hash256) ([]types.Hash256, error) {
@@ -420,39 +453,6 @@ func (s *Store) PrunableContractRoots(ctx context.Context, contractID types.File
 	}
 
 	return prunable, nil
-}
-
-// MarkUnrenewableContractsBad marks all contracts as bad that have a proof
-// height <= minProofHeight bad.
-func (s *Store) MarkUnrenewableContractsBad(ctx context.Context, minProofHeight uint64) error {
-	return s.transaction(ctx, func(ctx context.Context, tx *txn) error {
-		_, err := tx.Exec(ctx, `UPDATE contracts SET good = FALSE WHERE proof_height <= $1`, minProofHeight)
-		return err
-	})
-}
-
-// RejectPendingContracts marks all contracts as rejected that are currently
-// pending and have a formation height older than 'maxFormation'.
-func (s *Store) RejectPendingContracts(ctx context.Context, maxFormation time.Time) error {
-	return s.transaction(ctx, func(ctx context.Context, tx *txn) error {
-		_, err := tx.Exec(ctx, `UPDATE contracts SET state = $1 WHERE state = $2 AND formation < $3`,
-			sqlContractState(contracts.ContractStateRejected), sqlContractState(contracts.ContractStatePending), maxFormation)
-		return err
-	})
-}
-
-// SyncContract updates the contract with the given ID to the provided
-// parameters which are expected to contain information about the latest
-// revision of a contract.
-func (s *Store) SyncContract(ctx context.Context, contractID types.FileContractID, params contracts.ContractSyncParams) error {
-	return s.transaction(ctx, func(ctx context.Context, tx *txn) error {
-		_, err := tx.Exec(ctx, `
-UPDATE contracts
-SET capacity = $1, remaining_allowance = $2, revision_number = $3, size = $4, used_collateral = $5
-WHERE contract_id = $6
-`, params.Capacity, sqlCurrency(params.RemainingAllowance), params.RevisionNumber, params.Size, sqlCurrency(params.UsedCollateral), sqlHash256(contractID))
-		return err
-	})
 }
 
 func (tx *updateTx) UpdateContractElements(fces ...types.V2FileContractElement) error {
