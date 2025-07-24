8f2e432263b2193f02f62452efeaf3a06acb39fc
PJ <peterjan.brone@gmail.com>
2025-07-24 12:05:16 +0200

    testing: fix TestAccountsAPI

--- api/admin/admin_test.go
+++ api/admin/admin_test.go
@@ -29,6 +29,15 @@ func TestAccountsAPI(t *testing.T) {
 	c := testutils.NewConsensusNode(t, zap.NewNop())
 	indexer := testutils.NewIndexer(t, c, zap.NewNop())
 
+	// remove all existing accounts (slab related service accounts)
+	existing, err := indexer.Accounts(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, acc := range existing {
+		indexer.AccountsDelete(context.Background(), acc)
+	}
+
 	var accs []types.PublicKey
 	for range 10 {
 		accs = append(accs, types.GeneratePrivateKey().PublicKey())
@@ -38,7 +47,7 @@ func TestAccountsAPI(t *testing.T) {
 		}
 	}
 
-	err := indexer.AccountsAdd(context.Background(), accs[len(accs)-1])
+	err = indexer.AccountsAdd(context.Background(), accs[len(accs)-1])
 	if err == nil || !strings.Contains(err.Error(), accounts.ErrExists.Error()) {
 		t.Fatal("expected ErrExists", err)
 	}
