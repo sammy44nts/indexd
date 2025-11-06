5cd672516f10cc1a48ae088bdf90b744d479777d
Christopher Tarry <chrisjtarry@gmail.com>
2025-11-06 11:28:42 -0500

    check host has enough storage remaining during migration uploads

--- slabs/uploads.go
+++ slabs/uploads.go
@@ -15,8 +15,9 @@ import (
 )
 
 var (
-	errNotEnoughHosts = errors.New("not enough hosts")
-	errRootMismatch   = errors.New("sector root of shard doesn't match expected root")
+	errNotEnoughHosts   = errors.New("not enough hosts")
+	errRootMismatch     = errors.New("sector root of shard doesn't match expected root")
+	errNotEnoughStorage = errors.New("host doesn't have enough storage remaining for a sector")
 )
 
 type (
